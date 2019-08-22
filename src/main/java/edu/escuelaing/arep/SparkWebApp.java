782121ffc887c2787f222f9bbe98ad2dfa54f8fa
David <david.ramirez-mo@hotmail.com>
2019-08-22 11:18:08 -0500

    webpage

--- src/main/java/edu/escuelaing/arep/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arep/SparkWebApp.java
@@ -2,6 +2,11 @@ package edu.escuelaing.arep;
 
 import static spark.Spark.*;
 
+import javax.print.attribute.standard.Media;
+
+import spark.Request;
+import spark.Response;
+
 public class SparkWebApp {
 
     public static void main(String[] args) {
@@ -34,6 +39,35 @@ public class SparkWebApp {
             
             return MedianStandard.desviacion(list);
         });
+        get("/inputdata", (req, res) -> inputDataPage(req, res));
+        get("/results", (req, res) -> resultsPage(req, res));
+    }
+    private static String inputDataPage(Request req, Response res) {
+        String pageContent
+                = "<!DOCTYPE html>"
+                + "<html>"
+                + "<body>"
+                + "<h2>Median an Standard deviation Calculator</h2>"
+                + "<form action=\"/results\">"
+                + "  please insert the data separated by a blank space :<br>"
+                + "  <input type=\"text\" name=\"Numeros\" value=\"\">"
+                + "  <br>"
+                + "  <input type=\"submit\" value=\"Calculate\">"
+                + "</form>"
+                + "<p>Al select the  \"Calculate\" button to display the results in the page  \"/results\".</p>"
+                + "</body>"
+                + "</html>";
+        return pageContent;
+    }
+
+    private static String resultsPage(Request req, Response res) {
+        String[] values=req.queryParams("Numeros").split(" ");
+            LinkedList list=new LinkedList();
+            for(String i: values){
+                list.insertar(Double.parseDouble(i));
+            }
+        
+        return "The median of the inserted data is:"+ MedianStandard.media(list) +" and standard deviation is:"+MedianStandard.desviacion(list) ;
     }
 
     static int getPort() {
